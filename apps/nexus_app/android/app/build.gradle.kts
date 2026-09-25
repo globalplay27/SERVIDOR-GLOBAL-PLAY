7813c96b4d66e4e8fe3a2d939006774849dca2c8
globalplay27 <globalplaystreaming26@gmail.com>
2026-09-25 15:07:47 -0300

    fix(apk): compile mobile app against Android 36

--- apps/nexus_app/android/app/build.gradle.kts
+++ apps/nexus_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.nexus_ai"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
